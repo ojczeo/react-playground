c396b912655f31145f4a0b5460794cd33092f2e2
Andrzej <codelaborer@gmail.com>
2016-08-16 15:22:49 +0200

    react components

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+ }
+ update(e){
+   this.setState({txt: e.target.value})
+ }
+  render(){
+   return(
+    <div>
+    <input type="text"
+     onChange={this.update.bind(this)} />
+    <h1>{this.state.txt}</h1>
+    </div>
+  );
+ }
+}
+
+ReactDOM.render(
+  <App txt="this is the props vas" />,
+  document.getElementById('app')
+)
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isrequired
+}
+
+// class App extends React.Component {
+//   constructor(){
+//    super();
+//    this.state = { txt: 'this is the state txt'}
+//  }
+//  update(e){
+//   this.setState({})
+// }
+//   render(){
+//     return (
+//       <div>
+//         <h1>{this.state.txt}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// class App extends React.Component {
+//  render(){
+//    return React.createElement('h1', null, 'Hello Guys');
+//  }
+// }
+
+// const App = () => <h1>Hello Eggheads</h1>
+
+// export default App
+
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "this is the default txt"
+// }
+//
+// ReactDOM.render(<App txt/>, document.getElementById('app'));
