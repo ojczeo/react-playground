a5233e14f286425fe45473fc80100cbc9b65f31a
Andrzej <codelaborer@gmail.com>
2016-08-16 16:24:42 +0200

    mounting components

--- App.js
+++ App.js
@@ -2,101 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
+  }
+  update(){
+    this.setState({val: this.state.val +1 })
+  }
+  componentWillMount(){
+    console.log("mounting")
+  }
   render(){
-   return(
-    <Button> I <Heart/> React </Button>
-  );
- }
+    console.log('rendering!')
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentDidMount(){
+    console.log("mounted")
+  }
+  componentWillUnmount(){
+    console.log("bye!")
+  }
 }
 
-class Button extends React.Component {
-  render () {
+class Wrapper extends React.Component {
+  constructor(){
+    super();
+  }
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render() {
     return (
-      <button>{this.props.children}</button>
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
+      </div>
     );
   }
 }
 
-const Heart = () => <span className="glyphicon glypicon-heart">&#9825;</span>
-
-class Slider extends React.Component {
-  render (){
-    return (
-      <div>
-      <input type="range" ref="inp"
-      min="0"
-      max="255"
-      onChange={this.props.update} />
-      </div>
-    )
-  }
-}
-
-// const Widget = (props) => {
-//   return (
-//     <div>
-//       <input type="text"
-//         onChange={props.update} />
-//       <h1>{props.txt}</h1>
-//     </div>
-//   )
-// }
-
-ReactDOM.render(
-  <App txt="this is the props vas" />,
-  document.getElementById('app')
-)
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isrequired
-}
-
-
-// const Widget = (props) => {
-//   return (
-//     <div>
-//       <input type="text"
-//         onChange={props.update} />
-//       <h1>{props.txt}</h1>
-//     </div>
-//   )
-// }
-
-// class App extends React.Component {
-//   constructor(){
-//    super();
-//    this.state = { txt: 'this is the state txt'}
-//  }
-//  update(e){
-//   this.setState({})
-// }
-//   render(){
-//     return (
-//       <div>
-//         <h1>{this.state.txt}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// class App extends React.Component {
-//  render(){
-//    return React.createElement('h1', null, 'Hello Guys');
-//  }
-// }
-
-// const App = () => <h1>Hello Eggheads</h1>
-
-// export default App
-
-// App.propTypes = {
-//   txt: React.PropTypes.string,
-//   cat: React.PropTypes.number.isRequired
-// }
-//
-// App.defaultProps = {
-//   txt: "this is the default txt"
-// }
-//
-// ReactDOM.render(<App txt/>, document.getElementById('app'));
+export default App
